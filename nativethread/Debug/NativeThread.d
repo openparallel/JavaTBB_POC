d33b7566e21fd08d6a4659bf1b35635c6af072ec
Richard Parratt <rich.d.rich@gmail.com>
2011-08-29 16:23:58 +1200

    initial commit

---
+++ nativethread/Debug/NativeThread.d
@@ -0,0 +1,49 @@
+NativeThread.d NativeThread.o: ../NativeThread.cpp \
+  /System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni.h \
+  /System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni_md.h \
+  /usr/local/tbb30_20110704oss/include/tbb/task_scheduler_init.h \
+  /usr/local/tbb30_20110704oss/include/tbb/tbb_stddef.h \
+  /usr/local/tbb30_20110704oss/include/tbb/tbb_config.h \
+  /usr/local/tbb30_20110704oss/include/tbb/parallel_for.h \
+  /usr/local/tbb30_20110704oss/include/tbb/task.h \
+  /usr/local/tbb30_20110704oss/include/tbb/tbb_machine.h \
+  /usr/local/tbb30_20110704oss/include/tbb/machine/linux_intel64.h \
+  /usr/local/tbb30_20110704oss/include/tbb/machine/macos_common.h \
+  /usr/local/tbb30_20110704oss/include/tbb/partitioner.h \
+  /usr/local/tbb30_20110704oss/include/tbb/blocked_range.h \
+  /usr/local/tbb30_20110704oss/include/tbb/tbb_exception.h \
+  /usr/local/tbb30_20110704oss/include/tbb/tbb_allocator.h \
+  /usr/local/tbb30_20110704oss/include/tbb/blocked_range.h \
+  ../NativeThread.h
+
+/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni.h:
+
+/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers/jni_md.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/task_scheduler_init.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/tbb_stddef.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/tbb_config.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/parallel_for.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/task.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/tbb_machine.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/machine/linux_intel64.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/machine/macos_common.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/partitioner.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/blocked_range.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/tbb_exception.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/tbb_allocator.h:
+
+/usr/local/tbb30_20110704oss/include/tbb/blocked_range.h:
+
+../NativeThread.h:
